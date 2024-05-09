16a4902b9ced06a23758c02bf8b12af1a138ee74
mmfapp <118179050+mmfapp@users.noreply.github.com>
2024-05-09 15:23:59 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -22,3 +22,21 @@ class Auth:
                 return response.json()
 
         return WhatsApp(phone, self.base_url)
+
+    def call(self, phone):
+        class Call:
+            def __init__(self, phone, base_url):
+                self.phone = phone
+                self.base_url = base_url
+
+            def send(self):
+                response = requests.get(self.base_url + "call/" + self.phone)
+                return response.json()
+
+            def verify(self, code):
+                headers = {'Content-Type': 'application/json'}
+                data = json.dumps({'code': code})
+                response = requests.post(self.base_url + "call/" + self.phone, headers=headers, data=data)
+                return response.json()
+
+        return Call(phone, self.base_url)
